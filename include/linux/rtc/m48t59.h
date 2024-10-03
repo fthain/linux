4b9d9ef484335a993d7eea47614079403baab51e
Finn Thain <fthain@linux-m68k.org>
2024-10-04 09:06:52 +1000

    rtc: m48t59: Use platform_data for offset value
    Instead of a hard-coded value, put the YY offset into the platform_data
    struct so that other platforms can set other values without more ifdefs.
    
    Signed-off-by: Finn Thain <fthain@linux-m68k.org>
    ---
    Tested on qemu-system-sparc64. No behavioural change was observed.

--- include/linux/rtc/m48t59.h
+++ include/linux/rtc/m48t59.h
@@ -56,6 +56,9 @@ struct m48t59_plat_data {
 	void __iomem *ioaddr;
 	/* offset to RTC registers, automatically set according to the type */
 	unsigned int offset;
+
+	/* YY digits (in RTC) are offset, i.e. year = 1900 + YY + yy_offset */
+	time64_t yy_offset;
 };
 
 #endif /* _LINUX_RTC_M48T59_H_ */
